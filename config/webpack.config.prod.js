baa9751d23d46d944ca31adee6ed9f31474da10d
dulx <dulx96@gmail.com>
2018-06-24 22:41:12 +0700

    optimize image test, cache test

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -315,6 +315,7 @@ module.exports = {
     new webpack.DefinePlugin(env.stringified),
     // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
+      cache: true,
       compress: {
         warnings: false,
         // Disabled because of an issue with Uglify breaking seemingly valid code:
